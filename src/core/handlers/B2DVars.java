72770468b182a9a729f238a6e9fa325e6eb6948f
xZant <rafaeljr1995@gmail.com>
2016-03-26 23:56:28 -0400

    updated B2DVars.java and GameStateManager.java

--- src/core/handlers/B2DVars.java
+++ src/core/handlers/B2DVars.java
@@ -1,10 +1,13 @@
 package core.handlers;
 
 public class B2DVars {
-	
+
+	// box2d works better with fixed time steps
+	public static final float STEP = 1 / 60f;
+
 	// pixels per meter
 	public static final float PPM = 100;
-	
+
 	// categorybits
 	public static final short BIT_GROUND = 2;
 	public static final short BIT_BOX = 4;
--- src/core/handlers/GameStateManager.java
+++ src/core/handlers/GameStateManager.java
@@ -5,52 +5,70 @@ import java.util.Stack;
 import core.game.Game;
 import core.states.GameState;
 import core.states.Play;
-
+import core.states.PlayState;
 
 public class GameStateManager {
 
 	private Game game;
-	
+
 	private Stack<GameState> gamestates;
-	
+
 	public static final int PLAY = 1234;
-	
-	public GameStateManager(Game game){
+
+	// actual playstate
+	public static final int PLAYSTATE = 1337;
+
+	public GameStateManager(Game game) {
 		this.game = game;
 		gamestates = new Stack<GameState>();
-		pushState(PLAY); // set first state when initializing
+		//pushState(PLAYSTATE); // set first state when initializing
 	}
-	
-	public void update(float dt){
+
+	public void update(float dt) {
 		gamestates.peek().update(dt);
 	}
-	
-	public void render(){
+
+	public void render() {
 		gamestates.peek().render();
 	}
-	
-	public Game getGame(){
+
+	public Game getGame() {
 		return game;
 	}
-	
-	
-	// pushing and popping
-	private GameState getState(int state){
-		if (state == PLAY) return new Play(this);
-		return null;
-	}
-	
-	public void setState(int state){
-		popState();
+
+	/*
+	 * pops the current GameState in the stack, creates a new instance of the
+	 * given state, and pushes this into the stack.
+	 */
+	public void setState(int state) {
+		if (!gamestates.isEmpty())
+			popState();
 		pushState(state);
 	}
-	
-	public void pushState(int state){
+
+	// popState()
+	private void popState() {
+		GameState g = gamestates.pop();
+		System.out.println("disposing from gsm");
+		g.dispose();
+	}
+
+	// pushState
+	private void pushState(int state) {
 		gamestates.push(getState(state));
 	}
-	
-	public void popState(){
-		GameState g = gamestates.pop();
-		g.dispose();
+
+	// getState
+	private GameState getState(int state) {
+		if (state == PLAY) {
+			return new Play(this);
+		} else if (state == PLAYSTATE) {
+			// temp
+			// DummyPlayer dplayer = new DummyPlayer();
+			// SaveConfiguration temp = new SaveConfiguration(0, 0, dplayer);
+
+			return new PlayState(this);
+		}
+		return null;
 	}
 }
