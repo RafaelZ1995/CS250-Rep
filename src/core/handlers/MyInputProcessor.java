8132c495a81fef98e01b54f7bfa9182f0524a2a4
xZant <rafaeljr1995@gmail.com>
2016-03-26 23:47:57 -0400

    deleted stuff in MyInputProcessor that we forgot with Jason

--- src/core/handlers/MyInputProcessor.java
+++ src/core/handlers/MyInputProcessor.java
@@ -8,32 +8,22 @@ public class MyInputProcessor extends InputAdapter {
 	public boolean keyDown(int k) {
 		if(k == Keys.A) {
 			MyInput.setKey(MyInput.LEFT, true);
-			left = true;
-			System.out.println("A pressed " + left);
 		}
 		
 		if(k == Keys.D) {
 			MyInput.setKey(MyInput.RIGHT, true);
-			right = true;
-			System.out.println("D pressed " + right);
 		}
 		
 		if(k == Keys.W) {
 			MyInput.setKey(MyInput.UP, true);
-			up = true;
-			System.out.println("W pressed " + up);
 		}
 		
 		if(k == Keys.S) {
 			MyInput.setKey(MyInput.DOWN, true);
-			down = true;
-			System.out.println("S pressed " + down);
 		}
 		
 		if(k == Keys.SPACE) {
 			MyInput.setKey(MyInput.JUMP, true);
-			jump = true;
-			System.out.println("Spacebar pressed " + jump);
 		}
 		
 		return true;
@@ -42,31 +32,21 @@ public class MyInputProcessor extends InputAdapter {
 	public boolean keyUp(int k) {
 		if(k == Keys.A) {
 			MyInput.setKey(MyInput.LEFT, false);
-			left = false;
-			System.out.println("A released " + left);
 		}
 		if(k == Keys.D) {
 			MyInput.setKey(MyInput.RIGHT, false);
-			right = false;
-			System.out.println("D released " + right);
 		}
 		
 		if(k == Keys.W) {
 			MyInput.setKey(MyInput.UP, false);
-			up = false;
-			System.out.println("W released " + up);
 		}
 		
 		if(k == Keys.S) {
 			MyInput.setKey(MyInput.DOWN, false);
-			down = false;
-			System.out.println("S released " + down);
 		}
 		
 		if(k == Keys.SPACE) {
 			MyInput.setKey(MyInput.JUMP, false);
-			jump = false;
-			System.out.println("Spacebar released " + jump);
 		}
 		
 		return true;
