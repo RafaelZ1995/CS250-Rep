ad2d749c6196acdb9b3ce07b77f92e4ee1600ac3
Jason Vazquez-Li <vazquezlijason@gmail.com>
2016-04-27 01:42:32 -0400

    Create MusicHandler.java

---
+++ src/core/handlers/MusicHandler.java
@@ -0,0 +1,70 @@
+package core.handlers;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+
+import core.game.Game;
+
+public class MusicHandler implements ApplicationListener{
+
+	private Music music;
+	private com.badlogic.gdx.audio.Sound sound;
+	private PlayContactListener cl;
+	private GameStateManager gsm;
+	private MyInput myInput;
+	
+	public MusicHandler(GameStateManager gsm) {
+		
+		this.cl = Game.universe.getCurrentSector().getContactListener();
+		this.gsm = gsm;
+		
+	}
+	
+	@Override
+	public void create() {
+		
+		music = Gdx.audio.newMusic(Gdx.files.internal("Sound/Dreams of Above - Maze Master.mp3"));
+		sound = Gdx.audio.newSound(Gdx.files.internal("Sound/Jumping.mp3"));
+		
+		music.setLooping(true); // music will loop
+		music.setVolume(0.5f);
+		music.play();
+		
+		System.out.println("music created");
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		
+	}
+
+	@Override
+	public void render() {
+		
+		// play sound is W or SPACEBAR is pressed 
+		if(myInput.isPressed(4) == true && cl.isPlayerOnGround()) {
+			sound.play(0.25f);
+		}
+		
+	}
+
+	@Override
+	public void pause() {
+		
+	}
+
+	@Override
+	public void resume() {
+		
+	}
+
+	@Override
+	public void dispose() {
+		
+		music.dispose();
+		sound.dispose();
+		
+	}
+
+}
