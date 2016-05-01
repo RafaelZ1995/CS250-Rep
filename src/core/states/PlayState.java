176060a200aee4083a1c035e1e2315ecd38671f2
Jason Vazquez-Li <vazquezlijason@gmail.com>
2016-04-30 22:57:51 -0400

    Update PlayState.java
    made changes so HUD works

--- src/core/states/PlayState.java
+++ src/core/states/PlayState.java
@@ -40,6 +40,7 @@ public class PlayState extends GameState {
 		super(gsm);
 		this.universe = Game.universe;
 		this.saveConf = saveConf;
+		player = saveConf.getPlayer();
 		hud = new Hud(gsm);
 		
 		// HERE, you would set the Sector stored in saveConf.
@@ -49,7 +50,6 @@ public class PlayState extends GameState {
 
 		currentWorld = universe.getCurrentSector().getWorld();
 		// get the player reference from the SaveConfiguration we are loading
-		player = saveConf.getPlayer();
 
 	}
 
@@ -59,15 +59,15 @@ public class PlayState extends GameState {
 	public PlayState(GameStateManager gsm) {
 		super(gsm);
 		this.universe = Game.universe;
+		universe.setSector(0);
+		currentWorld = universe.getCurrentSector().getWorld(); // set sector
+		// before this
+		player = new Player(currentWorld, gsm);
 		hud = new Hud(gsm);
 		// since new game, set sector to 0 because we dont have a
 		// saveconfiguration to load,
 		// or we could automatically make a saveConfiguration that starts at
 		// sector 0 and everything.. this is probably better
-		universe.setSector(0);
-		currentWorld = universe.getCurrentSector().getWorld(); // set sector
-									// before this
-		player = new Player(currentWorld, gsm);
 		
 	}
 
@@ -80,16 +80,19 @@ public class PlayState extends GameState {
 	public void update(float dt) {
 		universe.update();
 		player.update();
+		hud.update();
 		// System.out.println(dplayer.getPlayerBody().getPosition());
 		// world step is done in Sector's update
 	}
 
 	@Override
 	public void render() {
+		
 		Gdx.gl20.glClear(GL20.GL_COLOR_BUFFER_BIT); // already done in Game?
 		// System.out.println("playstate rendering");
 		universe.render();
 		hud.render();
+		
 	}
 
 	@Override
